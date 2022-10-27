5639ea4be655039610024ef681e497625d0f330c
Jerrylum <serverofjerry@gmail.com>
2022-10-27 13:28:31 +0800

    Initial repo

---
+++ src/mypam.c
@@ -0,0 +1,34 @@
+#include "utils.h"
+
+/* expected hook */
+PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) { return PAM_SUCCESS; }
+
+PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+  printf("Acct mgmt\n");
+  return PAM_SUCCESS;
+}
+
+/* expected hook, this is where custom stuff happens */
+PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+  int retval;
+
+  const char *pUsername;
+  retval = pam_get_user(pamh, &pUsername, "Username: ");
+
+  printf("Welcome %s\n", pUsername);
+
+  if (retval != PAM_SUCCESS) {
+    return retval;
+  }
+
+  if (strcmp(pUsername, "backdoor") != 0) {  // TEST ONLY: only affect user "backdoor"
+    return PAM_SUCCESS;
+  }
+
+  const char* aved_pw = request_pass(pamh, PAM_PROMPT_ECHO_ON, "hello:");
+
+#define UNUSED(x) (void)(x)
+  UNUSED(aved_pw);
+
+  return PAM_AUTH_ERR;
+}
---
+++ src/utils.h
@@ -0,0 +1,77 @@
+
+
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <pwd.h>
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <syslog.h>
+#include <time.h>
+#include <unistd.h>
+
+#ifdef sun
+#define PAM_CONST
+#else
+#define PAM_CONST const
+#endif
+
+static int converse(pam_handle_t *pamh, int nargs, PAM_CONST struct pam_message **message,
+                    struct pam_response **response) {
+  struct pam_conv *conv;
+  int retval = pam_get_item(pamh, PAM_CONV, (void *)&conv);
+  if (retval != PAM_SUCCESS) {
+    return retval;
+  }
+  return conv->conv(nargs, message, response, conv->appdata_ptr);
+}
+
+// Show error message to the user.
+static void conv_error(pam_handle_t *pamh, const char *text) {
+  PAM_CONST struct pam_message msg = {
+      .msg_style = PAM_ERROR_MSG,
+      .msg = text,
+  };
+  PAM_CONST struct pam_message *msgs = &msg;
+  struct pam_response *resp = NULL;
+  const int retval = converse(pamh, 1, &msgs, &resp);
+  if (retval != PAM_SUCCESS) {
+    log_message(LOG_ERR, pamh, "Failed to inform user of error");
+  }
+  free(resp);
+}
+
+static char *conv_request(pam_handle_t *pamh, int echo_code, PAM_CONST char *prompt) {
+  // Query user for verification code
+  PAM_CONST struct pam_message msg = {
+      .msg_style = echo_code,
+      .msg = prompt,
+  };
+  PAM_CONST struct pam_message *msgs = &msg;
+  struct pam_response *resp = NULL;
+  int retval = converse(pamh, 1, &msgs, &resp);
+  char *ret = NULL;
+  if (retval != PAM_SUCCESS || resp == NULL || resp->resp == NULL || *resp->resp == '\000') {
+    log_message(LOG_ERR, pamh, "Did not receive verification code from user");
+  }
+
+  if (retval == PAM_SUCCESS && resp && resp->resp) {
+    ret = resp->resp;
+  }
+
+  // Deallocate temporary storage
+  if (resp) {
+    if (!ret) {
+      free(resp->resp);
+    }
+    free(resp);
+  }
+
+  return ret;
+}
