eefe174a7a09ead8923caacee8ce0ef3b72eecc7
Joe Lum <joelum-hk@proton.me>
2023-01-31 06:20:18 -0500

    backup

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Sandbox
-cd build && make && ./mysandbox
+# cd build && make && ./mysandbox
 
 # Deploy 
-# cd build && make && sudo cp libmypam.so /lib/security && echo installed
+cd build && make && sudo cp libmypam.so /lib/security && echo installed
+
+# auth required libmypam.so debug
